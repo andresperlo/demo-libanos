f4ded26d8d13f1411aa196fd1dfadce44b176314
andresperlo <andres_perlo@hotmail.com>
2020-08-24 19:01:57 -0300

    Updates

---
+++ precache-manifest.268f7624baa33e53890cdc20ad5f43eb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9054ca1345a3fdaf8aa66a240beb59df",
+    "url": "/demo-libanos/index.html"
+  },
+  {
+    "revision": "250c91d58db4957a2e1d",
+    "url": "/demo-libanos/static/css/main.40c32791.chunk.css"
+  },
+  {
+    "revision": "7d4f8c146c039a8bc23c",
+    "url": "/demo-libanos/static/js/2.75d877c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-libanos/static/js/2.75d877c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250c91d58db4957a2e1d",
+    "url": "/demo-libanos/static/js/main.8900ab83.chunk.js"
+  },
+  {
+    "revision": "279ea2638bd619bcfcbf",
+    "url": "/demo-libanos/static/js/runtime-main.f1a0232c.js"
+  },
+  {
+    "revision": "01fe7f291e5c1e260c2252a84badb1d6",
+    "url": "/demo-libanos/static/media/anana.01fe7f29.jpeg"
+  },
+  {
+    "revision": "85f1d0701aa974698c8d78bac24c8f3b",
+    "url": "/demo-libanos/static/media/arandanos.85f1d070.jpeg"
+  },
+  {
+    "revision": "6310d25e490cc65714522bfff4e6c037",
+    "url": "/demo-libanos/static/media/bananas.6310d25e.jpeg"
+  },
+  {
+    "revision": "b1c274676ee33d5823be604cc477c90f",
+    "url": "/demo-libanos/static/media/frutilla.b1c27467.jpeg"
+  },
+  {
+    "revision": "369268982e33dea069a8bfdaa3e83a5a",
+    "url": "/demo-libanos/static/media/granada.36926898.jpeg"
+  },
+  {
+    "revision": "ec11e7eb7e5f24fda722b30711aec7ae",
+    "url": "/demo-libanos/static/media/higos.ec11e7eb.jpeg"
+  },
+  {
+    "revision": "637a1b99b5365c9a7b56004da1a44b7b",
+    "url": "/demo-libanos/static/media/limon.637a1b99.jpeg"
+  },
+  {
+    "revision": "3e8a7f0e6f66f959da8b7c97cd2f1074",
+    "url": "/demo-libanos/static/media/moras.3e8a7f0e.jpeg"
+  },
+  {
+    "revision": "fdc39904fe90808a1908f748f30b7855",
+    "url": "/demo-libanos/static/media/naranja.fdc39904.jpeg"
+  },
+  {
+    "revision": "f217d6f4954dcd2cd87d4874d8e55e5e",
+    "url": "/demo-libanos/static/media/sandia.f217d6f4.jpeg"
+  },
+  {
+    "revision": "db0f42facebd4cbd3e929c0c0f4978cd",
+    "url": "/demo-libanos/static/media/uvas.db0f42fa.jpeg"
+  }
+]);
